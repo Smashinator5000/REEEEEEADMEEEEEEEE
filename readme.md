33ac63b4819b21c60abdce823507f39aaf4d2419
Ashley Bartholomew <ashley.52389@gmail.com>
2021-01-12 19:33:37 -0700

    final push

---
+++ readme.md
@@ -0,0 +1,19 @@
+# The title of my Readme is project furbaby
+ ## Eating tacos is best
+ 
+ ### Instructions for installation 
+ push the button 
+ #### usage 
+ it works 
+ ##### license 
+ yes 
+ ###### contributing 
+ undefined 
+ ### Tests 
+ didnt 
+ ### Questions 
+ questions 
+ ## Table of Contents 
+ 
+ 1. Title 
+ 2. Installation
--- script.js
+++ script.js
@@ -6,20 +6,51 @@ inquirer
     {
       type: 'input',
       name: 'name',
-      message: 'What is your name?',
+      message: 'What is the name of your project?',
     },
     {
-      type: 'checkbox',
-      message: 'What languages do you know?',
-      name: 'stack',
-      choices: ['HTML', 'CSS', 'JavaScript', 'MySQL'],
+      type: 'input',
+      message: 'How would you describe your project?',
+      name: 'Description',
     },
     {
-      type: 'list',
-      message: 'What is your preferred method of communication?',
-      name: 'contact',
-      choices: ['email', 'phone', 'telekinesis'],
+      type: 'input',
+      message: 'What are the installation instructions?',
+      name: 'installation',
     },
+    {
+      type: 'input',
+      message: 'What does the user need to know about its usage?',
+      name: 'usage',
+    },
+    {
+      type: 'input',
+      message: 'What is it licensed under?',
+      name: 'license',
+    },
+    {
+      type: 'input',
+      message: 'Who were the contributers?',
+      name: 'Contributing',
+    },
+    {
+      type: 'input',
+      message: 'How did you run the tests?',
+      name: 'tests',
+    },
+    {
+      type: 'input',
+      message: 'Where do I direct questions?',
+      name: 'questions',
+    },
+
   ]).then((response) => {
-      
+      console.table(response)
+      var template = `# The title of my Readme is ${response.name}\n ## Eating tacos is best\n \n ### Instructions for installation \n ${response.installation} \n #### usage \n ${response.usage} \n ##### license \n ${response.license} \n ###### contributing \n ${response.contributing} \n ### Tests \n ${response.tests} \n ### Questions \n ${response.questions} \n ## Table of Contents \n \n 1. Title \n 2. Installation`
+
+      fs.writeFile("readme.md", template, (err) => {
+        if(err) console.error(err)
+      })
   })
+
+  //title, table of content, Installation, Usage, License, Contributing, Tests, Questions
